1c2755efdaf33bf8cff64f5c8fb65c71557a75a2
Kri-web <krishnapriya@spericorn.com>
2022-09-30 14:48:21 +0530

    emojis

---
+++ server orginal/server.js
@@ -0,0 +1,26 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+//constant 
+const typeDefs = gql`
+  type Query {
+    greeting: String
+  },
+ 
+`;
+  
+//A resolver is a function that handles the data for each one of the fields of your schema. 
+
+const resolvers = {
+  Query: {
+    greeting: () => 'Hello Krishna Priya,😁👋',    
+  },
+
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+console.log("🥰🥰🥰🥰🥰🥰🥰🥰🥰");
+console.log("🤭🤭🤭🤭🤭🤭🤭🤭");
+console.log("🤗🤗🤗🤗🤗🤗🤗🤗🤗🤗");
+server
+  .listen({ port: 9000 })
+  .then(({ url }) => console.log(`Server running at 🤣 🤩 ${url}`))
